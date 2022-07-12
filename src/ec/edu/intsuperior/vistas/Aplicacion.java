9a24e054ae30d40c8783971a35a21e7fe6ad5440
Darwin1216 <alex-1208@live.com>
2022-07-12 01:57:30 -0500

    vercion 2 de dhurtado

--- src/ec/edu/intsuperior/vistas/Aplicacion.java
+++ src/ec/edu/intsuperior/vistas/Aplicacion.java
@@ -2,12 +2,23 @@
 
 package ec.edu.intsuperior.vistas;
 
+import ec.edu.intsuperior.modelo.Persona;
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 
 public class Aplicacion {
     public static void main(String[] args) {
-        JOptionPane.showInternalMessageDialog(null,"este es un segmento de dhurtado");
+        Scanner leer= new Scanner(System.in);
+        Persona p1= new Persona();
+        System.out.println("Ingresa la cedula");
+        p1.setCi(leer.next());
+        System.out.println("Ingresa nombre 1");
+        p1.setNombre1(leer.next());
+        p1.setNombre2(JOptionPane.showInputDialog("Ingresa tu  segundo nombre "));
+        p1.setApellido1(JOptionPane.showInputDialog("ingresa tu apellido paterno "));
+        
+        JOptionPane.showMessageDialog(null,p1.toString());
     }
             
     
