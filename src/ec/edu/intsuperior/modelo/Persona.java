3b141409e3ce0b27e768f70d63bf49770762db2d
DAVID <DAVID@DESKTOP-TC0PPM0>
2022-07-12 02:20:17 -0500

    version 2 de David Quiroz

--- src/ec/edu/intsuperior/modelo/Persona.java
+++ src/ec/edu/intsuperior/modelo/Persona.java
@@ -1,14 +1,115 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ec.edu.intsuperior.modelo;
 
-/**
- *
- * @author user
- */
+package ec.edu.intsuperior.modelo;
+import java.util.Date;
+
 public class Persona {
+    private String Ci;
+    private String nombre1;
+    private String nombre2;
+    private String apellido1;
+    private String apellido2;
+    private String direccion;
+    private String celular;
+    private String correo;
+    private Date   FechaNacimiento;
+
+    public Persona() {
+    }
+
+    public Persona(String Ci, String nombre1, String nombre2, String apellido1, String apellido2, String direccion, String celular, String correo, Date FechaNacimiento) {
+        this.Ci = Ci;
+        this.nombre1 = nombre1;
+        this.nombre2 = nombre2;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.direccion = direccion;
+        this.celular = celular;
+        this.correo = correo;
+        this.FechaNacimiento = FechaNacimiento;
+    }
+    
+    public void setCi(String Ci) {
+        this.Ci = Ci;
+    }
+
+    public void setNombre1(String nombre1) {
+        this.nombre1 = nombre1;
+    }
+
+    public void setNombre2(String nombre2) {
+        this.nombre2 = nombre2;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setFechaNacimiento(Date FechaNacimiento) {
+        this.FechaNacimiento = FechaNacimiento;
+    }
+
+    public String getCi() {
+        return Ci;
+    }
+
+    public String getNombre1() {
+        return nombre1;
+    }
+
+    public String getNombre2() {
+        return nombre2;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public Date getFechaNacimiento() {
+        return FechaNacimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Los datos guardados son: \n"
+                + "Cedula: "+getCi()+"\n"
+                + "Apellidos y Nombres: "+getApellido1()+" "+getApellido2()+ 
+                " "+getNombre1()+" "+getNombre2()+"\n"
+                + "Direccion: "+getDireccion()+"\n"
+                + "Cedular: "+getCelular()+"\n"
+                + "Correo: "+getCorreo();
+    }
+   
     
 }
--- src/ec/edu/intsuperior/vistas/Aplicacion.java
+++ src/ec/edu/intsuperior/vistas/Aplicacion.java
@@ -1,10 +1,26 @@
-
-
 package ec.edu.intsuperior.vistas;
+import ec.edu.intsuperior.modelo.Persona;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 
 public class Aplicacion {
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        Persona p1 = new Persona();
+        p1.setCi(JOptionPane.showInputDialog("Ingrese su numero de cedula"));
+        p1.setNombre1(JOptionPane.showInputDialog("Ingrese su primer nombre"));
+        p1.setNombre2(JOptionPane.showInputDialog("Ingresa su segundo nombre"));
+        p1.setApellido1(JOptionPane.showInputDialog("Ingresa el apellido paterno"));
+        p1.setApellido2(JOptionPane.showInputDialog("Ingresa el apellido materno"));
+        p1.setDireccion(JOptionPane.showInputDialog("Ingresa su direccion"));
+        p1.setCelular(JOptionPane.showInputDialog("Ingrese su numero celular"));
+        p1.setCorreo(JOptionPane.showInputDialog("Ingrese su correo"));
+        
+        JOptionPane.showMessageDialog(null, p1.toString());
+        
+        
+        
         
     }
             
