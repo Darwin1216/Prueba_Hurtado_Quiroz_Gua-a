d7e193cbdac69bf82efef4e16cd2cddb977f9abd
Darwin1216 <alex-1208@live.com>
2022-07-12 00:25:41 -0500

    vercion de dhurtado

--- src/ec/edu/intsuperior/vistas/Aplicacion.java
+++ src/ec/edu/intsuperior/vistas/Aplicacion.java
@@ -2,10 +2,12 @@
 
 package ec.edu.intsuperior.vistas;
 
+import javax.swing.JOptionPane;
+
 
 public class Aplicacion {
     public static void main(String[] args) {
-        
+        JOptionPane.showInternalMessageDialog(null,"estes es un segmento de dhurtado");
     }
             
     
