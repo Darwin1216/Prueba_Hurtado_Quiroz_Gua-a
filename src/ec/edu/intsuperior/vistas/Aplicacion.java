c7bb1ff92fa4c4d7c245c52f26b9329d5823150f
GuañaAnthony <sdemian2001@gmail.com>
2022-07-12 16:47:50 -0500

    Leccion de Anthony Guaña

--- src/ec/edu/intsuperior/vistas/Aplicacion.java
+++ src/ec/edu/intsuperior/vistas/Aplicacion.java
@@ -2,12 +2,17 @@
 
 package ec.edu.intsuperior.vistas;
 
+import javax.swing.JOptionPane;
+
 
 public class Aplicacion {
     public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Este es el segmento de aGuaña");
         
+            
+        }
     }
             
     
     
-}
+
